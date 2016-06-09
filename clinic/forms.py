e177a1815016020ed7a4b6dc9e2c4df3b01b9d78
david seroney <dvdseroney@gmail.com>
2016-06-09 16:25:21 +0300

    change dateofbirth field widget to SelectDateWidget

--- clinic/forms.py
+++ clinic/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Patients
+from django.forms import SelectDateWidget
+import datetime
 
 class PatientsForm(ModelForm):
 	class Meta:
 		model = Patients
 		fields = '__all__'
+		YEARCHOICES=(range(datetime.date.today().year - 100, datetime.date.today().year))
+		widgets = {
+            'dateofbirth': SelectDateWidget(years = YEARCHOICES),
+        }
