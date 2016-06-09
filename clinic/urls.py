34c1c09cb9f001539477475f50cbae45d165cde7
david seroney <dvdseroney@gmail.com>
2016-06-09 17:24:45 +0300

    you can't spell CRUD without C&D :)

--- clinic/urls.py
+++ clinic/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/patdetails/$', views.patdetails, name='patdetails'),
+    url(r'^(?P<pk>[0-9]+)/delete/$', views.delete, name='delete'),
+    url(r'^delete/$', views.deleted, name='deleted'),
+    url(r'^add/$', views.add, name='add'),
 ]
--- clinic/views.py
+++ clinic/views.py
@@ -34,3 +34,21 @@ def patdetails(request, pk):
     else:
         form = PatientsForm(instance = patients)
     return render(request, 'clinic/edit.html', {'form': form, 'patients': patients})
+
+def delete(request, pk):
+    patients = Patients.objects.get(pk = pk)
+    patients.delete()
+    return HttpResponseRedirect(reverse('clinic:deleted'))
+
+def deleted(request):
+    return render(request, 'clinic/deleted.html', {'notification':"PATIENT DELETED"})
+
+def add(request):
+    if request.method == "POST":
+        form = PatientsForm(request.POST)
+        if form.is_valid():
+            ourpatient=form.save()
+            return HttpResponseRedirect(reverse('clinic:index'))
+    else:
+        form = PatientsForm()
+    return render(request, 'clinic/edit.html', {'form': form})
